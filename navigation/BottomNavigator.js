6f7021cf3ff66c92fdde2c0ade4db8e055ee724b
shahzadbinshahjahan <shahzadbinshahjahan@gmail.com>
2024-04-06 22:08:47 +0530

    added icons for tab navigator

--- navigation/BottomNavigator.js
+++ navigation/BottomNavigator.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 import { Home, Learn, Marketplace, Community, Profile } from '../screens';
 import { useMemo } from 'react';
 
@@ -9,10 +10,30 @@ const Stack = createNativeStackNavigator();
 export default function BottomNavigator({initialRouteName}) {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-      <Tab.Screen name="Home" component={Home}/>
-      <Tab.Screen name="Learn" component={Learn} />
-      <Tab.Screen name="Marketplace" component={Marketplace} />
-      <Tab.Screen name="Community" component={Community} />
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarLabel:"Home",
+        tabBarIcon:({color,size})=>(
+          <Ionicons name="home" color={color} size={size}/>
+        )
+      }}/>
+      <Tab.Screen name="Learn" component={Learn} options={{
+        tabBarLabel:"Learn",
+        tabBarIcon:({color,size})=>(
+          <Ionicons name="book" color={color} size={size}/>
+        )
+      }}/>
+      <Tab.Screen name="Marketplace" component={Marketplace} options={{
+        tabBarLabel:"Marketplace",
+        tabBarIcon:({color,size})=>(
+          <Ionicons name="cart" color={color} size={size}/>
+        )
+      }}/>
+      <Tab.Screen name="Community" component={Community} options={{
+        tabBarLabel:"Community",
+        tabBarIcon:({color,size})=>(
+          <Ionicons name="people" color={color} size={size}/>
+        )
+      }}/>
     </Tab.Navigator>
   );
 }
