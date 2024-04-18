5dd050e60b9c96fc88903bb1a5d89633291cce4a
shahzadbinshahjahan <shahzadbinshahjahan@gmail.com>
2024-04-18 23:25:49 +0530

    fixed responsiveness in web browser

--- components/PageContent.jsx
+++ components/PageContent.jsx
@@ -10,7 +10,10 @@ export default function PageContent({header,imageSource}){
 
 const styles=StyleSheet.create({
     container:{
-        padding:"10%"
+        padding:"5%",
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
     },
     header:{
         textAlign:"center",
@@ -18,7 +21,7 @@ const styles=StyleSheet.create({
         fontWeight:"500"
     },
     heroImage:{
-        height:"100%",
+        flex:1,
         width:"100%"
     }
 })
